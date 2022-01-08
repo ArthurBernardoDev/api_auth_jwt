f8b6acc94b69ac5dab199836fa3a30185f111854
Arthur Bernardo <arthurutschgg@gmail.com>
2022-01-08 13:46:47 -0300

    feat: create register route

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,4 +1,5 @@
-import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
+import {Column, Entity, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate} from 'typeorm'
+import bcrypt from 'bcryptjs'
 
 @Entity('users')
 class User {
@@ -10,5 +11,11 @@ class User {
 
     @Column()
     password: string;
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword() {
+        this.password = bcrypt.hashSync(this.password, 8)
+    }
 }
 export default User
