5851ad86704e3acbef08ad29633f3a7fa4e0ee18
Arthur Bernardo <arthurutschgg@gmail.com>
2022-01-08 13:27:40 -0300

    feat: create model from users

---
+++ src/app/models/User.ts
@@ -0,0 +1,14 @@
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
+
+@Entity('users')
+class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
+export default User
